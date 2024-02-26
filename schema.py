16d1f8ff6d46fe41b13fbf3d0dea85722befd2e8
Erik Fekete <rikifekete2003@gmail.com>
2024-02-26 13:58:15 +0000

    Styling and stuff

--- schema.py
+++ schema.py
@@ -133,49 +133,6 @@ companies = pd.read_csv('SP_500.csv')
 
 tickers = companies['Symbol'].unique()
 
-'''def get_news_for_ticker(ticker, topic, sort, days_ago, limit):
-    api_key = "QC6PHJMTIZHC8S6B"
-    time_from = (datetime.utcnow() - timedelta(days=days_ago)).strftime('%Y%m%dT%H%M')
-    
-    # Construct the API URL with the dynamic 'time_from'
-    url = f'https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers={ticker}&topics={topic}&sort={sort}&time_from={time_from}&limit={limit}&apikey={api_key}'
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # This will raise an exception for HTTP errors
-        data = response.json()
-        news_items = format_news_feed_data_for_ticker(data, ticker)
-        return news_items
-    except requests.exceptions.HTTPError as errh:
-        logging.error("Http Error: %s", errh)
-    except requests.exceptions.ConnectionError as errc:
-        logging.error("Error Connecting: %s", errc)
-    except requests.exceptions.Timeout as errt:
-        logging.error("Timeout Error: %s", errt)
-    except requests.exceptions.RequestException as err:
-        logging.error("Other Error: %s", err)
-
-def format_news_feed_data_for_ticker(data, ticker):
-    articles = []
-    for item in data.get('feed', []):
-        try:
-            publishedTime = datetime.strptime(item.get('time_published'), "%Y%m%dT%H%M%S")
-        except ValueError:
-            publishedTime = 'N/A'  # Handle incorrect format gracefully
-        article = {
-            "tickerID": ticker,
-            "url": item.get('url', 'N/A'),
-            "publishedTime": publishedTime,
-            "summary": item.get('summary', 'N/A'),
-            "bannerImageURL": item.get('banner_image', 'N/A'),
-            "source": item.get('source', 'N/A'),
-            "category": item.get('category_within_source', 'N/A'),
-            "sourceDomain": item.get('source_domain', 'N/A'),
-            "overallSentiment": item.get('overall_sentiment_label', 'N/A')
-        }
-        articles.append(article)
-    return articles
-'''
-
 def get_news(tickers, topic, sort, days_ago, limit):
     api_key = "QC6PHJMTIZHC8S6B"
     time_from = (datetime.utcnow() - timedelta(days=days_ago)).strftime('%Y%m%dT%H%M')
@@ -458,7 +415,7 @@ def dbinit():
             db.session.add(Company(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
             db.session.commit()
     
-    '''for tickerOverview in tickers:
+    for tickerOverview in tickers:
         overview = get_company_overview(tickerOverview)
         if overview != 0:
             db.session.add(FinancialData(tickerOverview, datetime.now(), overview["MarketCapitalization"], overview["PERatio"], overview["EPS"], overview["ROE"]))
@@ -528,7 +485,6 @@ def dbinit():
                             sentiments = ArticleTickers(news_id, sentiment["ticker"], sentiment["relevanceScore"], sentiment["sentimentScore"])
                             db.session.add(sentiments)
             db.session.commit()
-    '''
 
 
 def split_primary_exchanges(json_data):
