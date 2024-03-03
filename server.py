0e7c7a6acf9bac43b7a03e4c6cec6d84960c5503
Erik Fekete <rikifekete2003@gmail.com>
2024-03-03 17:08:19 +0000

    React w/ Flask

--- server.py
+++ server.py
@@ -8,7 +8,7 @@
     #rikifekete2003@gmail.com - erikpwd (mine)
 
 import os
-from flask import Flask, request, redirect, render_template, url_for, session, send_from_directory
+from flask import Flask, request, redirect, render_template, url_for, session, send_from_directory, jsonify
 app = Flask(__name__, static_url_path="/static")
 app.secret_key = 'incredibly secret key of ours'
 from datetime import datetime, timedelta
@@ -78,49 +78,6 @@ def frequentUpdates():
                 currentCurrent.volumeOfTrade = stockPriceList["Current Volume"]
                 db.session.commit()
 
-    #TO REWRITE INTO UPDATES INSTEAD OF INSERTIONS, LIKE JUST ABOVE (above code works)
-        '''for tickerStock in tickers:
-            stockPriceList = get_current_stock_price_and_volume(tickerStock)
-            if stockPriceList != 0:
-                db.session.add(CurrentStockPrice(tickerStock, datetime.now(), stockPriceList["Current Price"], stockPriceList["Current Volume"]))
-                db.session.commit()
-        topGainersLosersDict = getTopGainersLosers()
-        topGainers = topGainersLosersDict["top_gainers"]
-        topLosers = topGainersLosersDict["top_losers"]
-        activelyTraded = topGainersLosersDict["most_actively_traded"]
-        for gainers in topGainers:
-            if gainers != None and gainers != 0:
-                print(gainers)
-                percent_float = float(gainers["change_percentage"].rstrip('%'))
-                db.session.add(TopGainers(gainers["ticker"], gainers["price"], gainers["change_amount"], percent_float, gainers["volume"]))
-                db.session.commit()
-        
-        for losers in topLosers:
-            if losers != None and losers != 0:
-                print(losers)
-                percent_float = float(losers["change_percentage"].rstrip('%'))
-                db.session.add(TopLosers(losers["ticker"], losers["price"], losers["change_amount"], percent_float, losers["volume"]))
-                db.session.commit()
-
-        for active in activelyTraded:
-            if active != None and active != 0:
-                print(active)
-                percent_float = float(active["change_percentage"].rstrip('%'))
-                db.session.add(ActivelyTraded(active["ticker"], active["price"], active["change_amount"], percent_float, active["volume"]))
-                db.session.commit()
-        
-        split_markets = split_primary_exchanges(get_global_market())
-        marketList = split_markets
-        for market in marketList:
-            print(market)
-            if market != 0 and market != None:
-                open_time = datetime.strptime(market['local_open'], "%H:%M").time()
-                close_time = datetime.strptime(market['local_close'], "%H:%M").time()
-                db.session.add(GlobalMarket(market["market_type"], market["region"], market["primary_exchanges"], open_time, close_time, market["current_status"], market["notes"]))
-                db.session.commit()
-                print("Inserted into Global Markets table!")
-        print("Frequent update initiated.")'''
-
 def dailyUpdates():
     #UPDATE FINANCIAL DATA TABLE
     print("Daily update initiated.")
@@ -131,27 +88,57 @@ scheduler.add_job(frequentUpdates, 'interval', minutes=5)
 scheduler.add_job(dailyUpdates, 'interval', hours=24)
 
 
+@app.route('/checkLoggedIn')
+def check_logged_in():
+    if 'username' in session:
+        return jsonify({'loggedIn': True})
+    else:
+        return jsonify({'loggedIn': False})
+
 @app.route('/')
 def home():
-    '''
-    if 'username' in session:
-        username = session['username']
-        return render_template('index.html', username=username)
-    else:
-        return redirect('/login')
-    '''
     return send_from_directory('static', 'index.html')
 
-@app.route('/login')
+
+@app.route('/Homepage')
+def homepage():
+    feed_entries = [
+    {
+        "headline": "Microsoft topples Apple to become global market cap leader",
+        "source": "finance.yahoo.com",
+        "time": "18 hours ago",
+        "icon": "/microsoftnews.png",
+        "logo": "/microsoftIcon.png",
+        "company": "Microsoft"
+    },
+    {
+        "headline": "OpenAI's Breakthrough in Generative AI",
+        "source": "techcrunch.com",
+        "time": "1 week ago",
+        "icon": "/openAInews.png",
+        "logo": "/openAIIcon.png",
+        "company": "OpenAI"
+    },
+    {
+        "headline": "Tesla Electric Vehicles Surge in Popularity",
+        "source": "theverge.com",
+        "time": "2 days ago",
+        "icon": "/teslanews.png",
+        "logo": "/teslaIcon.png",
+        "company": "Tesla"
+    }]
+    return jsonify(feed_entries)
+
+'''@app.route('/login')
 def loginpage():
     if 'username' in session:
         return redirect('/')
-    return render_template('login.html')
+    return render_template('login.html')'''
 
 @app.route('/submitlogin', methods=['POST', 'GET'])
 def login():
     
-    if request.method == 'POST':
+    '''if request.method == 'POST':
         email = request.form.get("email")
         email = escape(email)
         passwd = request.form.get("password")
@@ -199,7 +186,36 @@ def login():
               session.clear()
               return '<p>Your account is not activated. Another email was sent to you to verify your address</p><br /><p>The email you entered is {}<br /><p>Activate your account in an hour. If you cant, you can receive a new confirmation email on a login attempt</p><br /><form action="/login"><input type="submit" value="Return"></form>'.format(email)
             elif useractivated == 1:
-              return redirect('/')
+              return redirect('/')'''
+    data = request.json
+    email = data.get('username')
+    passwd = data.get('password')
+    users = Users.query.filter_by(email=email).first()
+    useractivated = users.isactivated
+    if users == None:
+        response_message = 'deny'
+    if not security.check_password_hash(users.hashed_password, passwd): 
+        response_message = 'deny'
+    else:
+        if useractivated == 0:
+            '''token = srializer.dumps(email, salt='email-confirm')
+            link = url_for('confirmemail', token=token, external=True)
+            urlbeginning = request.base_url
+            urlnew = urlbeginning+link
+            urlnew = urlnew.replace("/submitlogin", "")
+            #print("URL to receive by person signing up is: "+urlnew)
+            msgbody = 'Your new verification link is {}'.format(urlnew)
+            message = f"Subject: Email verification\n\n{msgbody}"
+            yag.send(to=email, contents=[message])
+            users.temptoken = token
+            db.session.commit()'''
+            response_message = 'deny'
+        else:
+            response_message = 'accept'
+            session['username'] = email
+            
+    return jsonify({'message': response_message})
+    
 
 @app.route('/logout')
 def logout():
@@ -209,7 +225,7 @@ def logout():
         pass
     session.pop('username', None)
     session.clear()
-    return redirect('/login')
+    return jsonify({'message': 'Logout successful'}), 200
 
 @app.route('/signup')
 def signup():
